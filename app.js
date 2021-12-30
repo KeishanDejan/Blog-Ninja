aacd298bc36d545bc3f37b23621471a79ade08e0
KeishanDejan <keishandejan@gmail.com>
2021-12-30 12:33:40 +0530

    POST method

--- app.js
+++ app.js
@@ -1,33 +1,56 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blogs')
 
 // express app
 const app = express();
 
+// connect to mongodb
+const dbURI = 'mongodb+srv://KeishanDejan:Maybe_better@nodetuts.yls5e.mongodb.net/node-tuts?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then((result) => app.listen(3000, () => {
+        console.log('Connected to Database...');
+        console.log("Server has started...");
+    }))
+    .catch((err) => console.log(err))
+
 // register view engine
 app.set('view engine', 'ejs');
 
-// listening to requests
-app.listen(3000, () => {
-    console.log("Server has started...");
-})
-
-
 // middlewares and static files
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true,}))
 app.use(morgan('dev'));
+
 
 // response for "/"
 app.get('/', (req, res) => {
-    const blogs = [
-        {title: 'Yoshi finds egg', snippet: 'Lorem ipsum dolar sit amet consectetur'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolar sit amet consectetur'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolar sit amet consectetur'}
-    ]
-    // res.send('<h1>Hello Ninjas</h1>');
-    // res.sendFile('./views/index.html', {root: __dirname});
-    res.render('index', { title: 'Home', blogs });
+    res.redirect('/blogs')
 });
+
+app.get('/blogs', (req,res) => {
+    Blog.find().sort({ createdAt : -1 })
+        .then((result) => {
+            res.render('index', { title: 'All Blogs', blogs: result})
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
+
+app.post('/blogs', (req,res) => {
+    const blog = new Blog(req.body);
+
+    blog.save()
+        .then((result) => {
+            res.redirect('/blogs')
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    console.log(req.body)
+})
 
 // response for '/about'
 app.get('/about', (req, res) => {
